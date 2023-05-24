6d707cbbf9bf56985f682ca6820876dfd508c1b4
Sterling <sterlingbarton81@gmail.com>
2023-05-24 11:37:55 -0600

    completed lab

--- server/app.py
+++ server/app.py
@@ -14,25 +14,66 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     return '<h1>Bakery GET API</h1>'
 
+
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_dict = [bakery.to_dict() for bakery in bakeries]
+
+    response = make_response(
+        jsonify(bakeries_dict),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(
+        id=id).first()
+    # filter(Bakery.id == id)
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        jsonify(bakery_dict),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price).all()
+    baked_goods_by_price_dict = [baked_goods.to_dict()
+                                 for baked_goods in baked_goods_by_price]
+
+    response = make_response(
+        jsonify(baked_goods_by_price_dict),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    most_expensive_dict = most_expensive.to_dict()
+
+    response = make_response(
+        jsonify(most_expensive_dict),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -13,14 +13,14 @@ with app.app_context():
 
     BakedGood.query.delete()
     Bakery.query.delete()
-    
+
     bakeries = []
     for i in range(20):
         b = Bakery(
             name=fake.company()
         )
         bakeries.append(b)
-    
+
     db.session.add_all(bakeries)
 
     baked_goods = []
@@ -34,7 +34,7 @@ with app.app_context():
 
         bg = BakedGood(
             name=name,
-            price=randint(1,10),
+            price=randint(1, 10),
             bakery=rc(bakeries)
         )
 
@@ -42,7 +42,7 @@ with app.app_context():
 
     db.session.add_all(baked_goods)
     db.session.commit()
-    
+
     most_expensive_baked_good = rc(baked_goods)
     most_expensive_baked_good.price = 100
     db.session.add(most_expensive_baked_good)
